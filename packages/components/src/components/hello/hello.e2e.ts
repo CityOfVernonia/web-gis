5b956d1d82e109c7cac6905cf02deaeb814ff41d
City of Vernonia GIS <gis@vernonia-or.gov>
2025-10-30 13:49:52 -0700

    slots don't have class

--- packages/components/src/components/hello/hello.e2e.ts
+++ packages/components/src/components/hello/hello.e2e.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
 describe('cov-hello', () => {
-  describe('fake success test test', () => {
+  describe('fake success test', () => {
     it('should not break math', () => {
       expect(1).toEqual(1);
     });
