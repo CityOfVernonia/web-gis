df86a2383341182139e1a80ffbd42b534c3c1a1a
City of Vernonia GIS <gis@vernonia-or.gov>
2025-11-06 10:23:38 -0800

    Update map-control-2d.tsx

--- packages/components/src/components/map-control-2d/map-control-2d.tsx
+++ packages/components/src/components/map-control-2d/map-control-2d.tsx
@@ -1,5 +1,5 @@
 import { createRef } from 'lit-html/directives/ref.js';
-import { JsxNode, LitElement, h, state } from '@arcgis/lumina';
+import { JsxNode, LitElement, h, property, state } from '@arcgis/lumina';
 import { CSS } from './resources';
 import { styles } from './map-control-2d.scss';
 import { guid } from '../../utils/guid';
@@ -9,7 +9,6 @@ import {
   supported as geolocationSupported,
 } from '@arcgis/core/widgets/support/geolocationUtils.js';
 import HomeViewModel from '@arcgis/core/widgets/Home/HomeViewModel';
-import ZoomViewModel from '@arcgis/core/widgets/Zoom/ZoomViewModel';
 import Point from '@arcgis/core/geometry/Point';
 import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import Graphic from '@arcgis/core/Graphic';
@@ -20,6 +19,23 @@ declare global {
   }
 }
 
+const IDS = {
+  home: guid(),
+  fullscreen: guid(),
+  locate: guid(),
+  rotation: guid(),
+  zoomIn: guid(),
+  zoomOut: guid(),
+};
+
+const canZoom = (type: 'in' | 'out', view?: __esri.MapView): boolean => {
+  if (!view || !view?.ready) return false;
+
+  return type === 'in'
+    ? view.scale > view.constraints?.effectiveMaxScale
+    : view.scale < view.constraints?.effectiveMinScale;
+};
+
 export class MapControl2D extends LitElement {
   //#region Static Members
 
@@ -29,17 +45,11 @@ export class MapControl2D extends LitElement {
 
   //#region Private Properties
 
-  private rotationActionRef = createRef<HTMLCalciteActionElement>();
-
-  private guid = guid();
-
   private homeViewModel = new HomeViewModel();
 
   private locateGraphic = new Graphic();
 
-  private view?: __esri.MapView;
-
-  private zoomViewModel = new ZoomViewModel();
+  private rotationActionRef = createRef<HTMLCalciteActionElement>();
 
   //#endregion
 
@@ -53,61 +63,48 @@ export class MapControl2D extends LitElement {
 
   //#endregion
 
+  //#region Public Properties
+
+  @property({ reflect: true }) view?: __esri.MapView;
+
+  //#endregion
+
   //#region Lifecycle
 
-  /**
-   * All this is just a proof of concept.
-   */
   load(): void {
-    // TODO: create controller to bind view models to component
-    // (import AccessorController from '@arcgis/lumina/controllers/accessor';)
-    const { homeViewModel, view, zoomViewModel } = this;
+    if (!this.view) {
+      const arcgisMap = this.el.parentElement as HTMLArcgisMapElement | null;
 
-    if (view) {
-      homeViewModel.view = view;
+      if (!arcgisMap || (arcgisMap && arcgisMap.tagName !== 'ARCGIS-MAP')) return;
 
-      zoomViewModel.view = view;
-    } else {
-      const parentElement = this.el.parentElement as HTMLArcgisMapElement | null;
-
-      if (!parentElement || (parentElement && parentElement.tagName !== 'ARCGIS-MAP')) return;
-
-      this.view = parentElement.view;
-
-      homeViewModel.view = this.view;
-
-      zoomViewModel.view = this.view;
+      this.view = arcgisMap.view;
     }
 
-    // TODO: create a controller to add and remove listener when component connects/disconnects
-    // import GenericController from '@arcgis/lumina/controllers';
-    if (document.fullscreenElement) this.fullscreenActive = true;
-
-    document.addEventListener('fullscreenchange', (): void => {
-      this.fullscreenActive = document.fullscreenElement ? true : false;
-    });
-
-    // TODO: create a locate view model and create controller to bind it to component
-    if (geolocationSupported() === false) this.locateState = 'disabled';
-
-    if (this.view) {
-      // TODO: logic in controller to create and remove handle
-      watch(
-        (): number => this.view.rotation,
-        (rotation: number): void => {
-          const icon = this.rotationActionRef.value.shadowRoot.querySelector(
-            '.icon-container',
-          ) as HTMLCalciteIconElement;
-
-          icon.style.transform = `rotate(${rotation}deg)`;
-        },
-      );
-    }
+    this._initView(this.view);
   }
 
   //#endregion
 
   //#region Private Methods
+
+  private _initView(view: __esri.MapView): void {
+    this.homeViewModel.view = view;
+
+    watch(
+      (): number => this.view.rotation,
+      (rotation: number): void => {
+        (
+          this.rotationActionRef.value.shadowRoot.querySelector('.icon-container') as HTMLCalciteIconElement
+        ).style.transform = `rotate(${rotation}deg)`;
+      },
+    );
+
+    if (geolocationSupported() === false) this.locateState = 'disabled';
+
+    document.addEventListener('fullscreenchange', (): void => {
+      this.fullscreenActive = document.fullscreenElement ? true : false;
+    });
+  }
 
   private fullscreen(): void {
     if (this.fullscreenActive) {
@@ -171,6 +168,20 @@ export class MapControl2D extends LitElement {
     view.rotation = 0;
   }
 
+  private zoomIn(): void {
+    const { view } = this;
+
+    // @ts-expect-error not typed
+    if (canZoom('in', view)) view.mapViewNavigation.zoomIn();
+  }
+
+  private zoomOut(): void {
+    const { view } = this;
+
+    // @ts-expect-error not typed
+    if (canZoom('out', view)) view.mapViewNavigation.zoomOut();
+  }
+
   //#endregion
 
   //#region Rendering
@@ -182,28 +193,26 @@ export class MapControl2D extends LitElement {
       <div class={CSS.actionBars}>
         <calcite-action-bar expand-disabled="" floating>
           <calcite-action
-            disabled={!this.zoomViewModel.canZoomIn}
+            disabled={!canZoom('in', this.view)}
             icon="plus"
-            id={`${this.guid}_zoom_in`}
+            id={IDS.zoomIn}
             scale="s"
             text="Zoom in"
-            // why the need to bind to itself???
-            onClick={this.zoomViewModel.zoomIn.bind(this.zoomViewModel)}
+            onClick={this.zoomIn}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_zoom_in`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.zoomIn}>
             Zoom in
           </calcite-tooltip>
 
           <calcite-action
-            disabled={!this.zoomViewModel.canZoomOut}
+            disabled={!canZoom('out', this.view)}
             icon="minus"
-            id={`${this.guid}_zoom_out`}
+            id={IDS.zoomOut}
             scale="s"
             text="Zoom out"
-            // why the need to bind to itself???
-            onClick={this.zoomViewModel.zoomOut.bind(this.zoomViewModel)}
+            onClick={this.zoomOut}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_zoom_out`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.zoomOut}>
             Zoom out
           </calcite-tooltip>
         </calcite-action-bar>
@@ -212,37 +221,37 @@ export class MapControl2D extends LitElement {
           <calcite-action
             disabled={this.homeViewModel.state === 'disabled'}
             icon="home"
-            id={`${this.guid}_default_extent`}
+            id={IDS.home}
             scale="s"
             text="Default extent"
-            onClick={this.homeViewModel.go}
+            onClick={this.homeViewModel.go.bind(this.homeViewModel)}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_default_extent`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.home}>
             Default extent
           </calcite-tooltip>
 
           <calcite-action
             hidden={!this.view || (this.view && !this.view.constraints.rotationEnabled)}
             icon="compass-needle"
-            id={`${this.guid}_reset_orientation`}
+            id={IDS.rotation}
             ref={this.rotationActionRef}
             scale="s"
             text="Reset orientation"
             onClick={this.resetRotation}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_reset_orientation`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.rotation}>
             Reset orientation
           </calcite-tooltip>
 
           <calcite-action
             disabled={this.locateState === 'disabled'}
             icon={this.locateState === 'locating' ? 'gps-on-f' : 'gps-on'}
-            id={`${this.guid}_zoom_to_location`}
+            id={IDS.locate}
             scale="s"
             text="Zoom to location"
             onClick={this.locate}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_zoom_to_location`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.locate}>
             Zoom to location
           </calcite-tooltip>
         </calcite-action-bar>
@@ -251,12 +260,12 @@ export class MapControl2D extends LitElement {
           <calcite-action
             disabled={this.fullscreenDisabled}
             icon={this.fullscreenActive ? 'full-screen-exit' : 'extent'}
-            id={`${this.guid}_fullscreen`}
+            id={IDS.fullscreen}
             scale="s"
             text={fullscreenText}
             onClick={this.fullscreen}
           ></calcite-action>
-          <calcite-tooltip close-on-click="" referenceElement={`${this.guid}_fullscreen`}>
+          <calcite-tooltip close-on-click="" referenceElement={IDS.fullscreen}>
             {fullscreenText}
           </calcite-tooltip>
         </calcite-action-bar>
